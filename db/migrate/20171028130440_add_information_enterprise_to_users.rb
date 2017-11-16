49e6864aaa8dff262b5cd478445c7b30b4c6559a
Thomasl <thomas.levot@lamache.org>
2017-11-16 08:11:31 +0000

    Création page inscription, connexion et modification

--- db/migrate/20171028130440_add_information_enterprise_to_users.rb
+++ db/migrate/20171028130440_add_information_enterprise_to_users.rb
@@ -1,7 +1,7 @@
 class AddInformationEnterpriseToUsers < ActiveRecord::Migration
   def change
     add_column :users, :social_reason, :string
-    add_column :users, :street, :string
+    add_column :users, :address, :string
     add_column :users, :city, :string
     add_column :users, :phone, :string
   end
